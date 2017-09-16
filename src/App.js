0146f95989e275a00de9a969716eb99857c4b205
James Rutledge <jarutledge2@gmail.com>
2017-09-16 03:41:25 -0400

    axios does not support jsonp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props, context) { console.log(`constructor`)
+    super(props, context)
+
+    // called before the component is mounted
+    // initialize state here
+  }
+
+  componentWillMount() { console.log(`componentWillMount`)
+    // invoked once
+    // fires before first render() (no DOM yet)
+  }
+
+  componentDidMount() { console.log(`componentDidMount`)
+    // invoked once (client-side only)
+    // good for AJAX: fetch, ajax, or subscriptions
+    // fires before first render()
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) { console.log(`componentWillReceiveProps`)
+    // invoked every time component receives new props
+    // does not fire before first render()
+    // use setState() here
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) { console.log(`shouldComponentUpdate`)
+    // skips render() if returns false
+    // invoked before every update (new props or state)
+    // does not fire before first render()
+  }
+
+  componentWillUpdate(nextProps, nextState, nextContext) { console.log(`componentWillUpdate`)
+    // invoked immediately before update (new props or state)
+    // does not fire before first render()
+    // can't use setState() here
+  }
+
+  componentDidUpdate(prevProps, prevState, prevContext) { console.log(`componentDidUpdate`)
+    // invoked immediately after DOM updates
+    // does not fire after first render()
+  }
+
+  componentWillUnmount() { console.log(`componentWillUnmount`)
+    // invoked before DOM removal
+  }
+
+
+  render() { console.log(`render`)
     return (
       <div className="App">
         <div className="App-header">
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Main.css';
 import Lists from './todo/Lists';
 
 class Main extends Component {
-  render() {
+  constructor(props, context) { console.log(`Main constructor`)
+    super(props, context)
+
+    this.state = { lists: [] }
+
+    // called before the component is mounted
+    // initialize state here
+  }
+
+  componentDidMount() { console.log(`Main componentDidMount`)
+    // invoked once (client-side only)
+    // good for AJAX: fetch, ajax, or subscriptions
+    // fires before first render()
+
+    // axios.get('http://localhost:1337/lists', {
+    //     responseType:'json',
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json',
+    //     }
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+
+  }
+
+  render() { console.log(`Main render`)
     return (
       <div className="Main">
         <div className="Main-header">
